8265ecbfa430624f88f596d73d854aa724371c67
Ferns <jzhan357@ucsc.edu>
2023-03-08 21:25:46 -0800

    Change to using brkint + noflsh

--- ectf_tools/device.py
+++ ectf_tools/device.py
@@ -146,9 +146,9 @@ async def load_hw(
 
     # Try to connect to the serial port
     logger.info(f"Connecting to serial port {dev_serial}...")
-    ser = Serial(dev_serial, 115200, timeout=2, inter_byte_timeout=0)
     if platform == "linux":
-        subprocess.run(["stty", "-F", dev_serial, "ignbrk", "echo"])
+        subprocess.run(["stty", "-F", dev_serial, "brkint", "noflsh"])
+    ser = Serial(dev_serial, 115200, timeout=2, inter_byte_timeout=0)
     ser.reset_input_buffer()
     logger.info(f"Connection opened on {dev_serial}")
 
@@ -237,9 +237,9 @@ async def load_sec_hw(
 
     # Try to connect to the serial port
     logger.info(f"Connecting to serial port {dev_serial}...")
-    ser = Serial(dev_serial, 115200, timeout=2, inter_byte_timeout=0)
     if platform == "linux":
-        subprocess.run(["stty", "-F", dev_serial, "ignbrk", "echo"])
+        subprocess.run(["stty", "-F", dev_serial, "brkint", "noflsh"])
+    ser = Serial(dev_serial, 115200, timeout=2, inter_byte_timeout=0)
     ser.reset_input_buffer()
     logger.info(f"Connection opened on {dev_serial}")
 
@@ -303,12 +303,13 @@ async def mode_change(
     logger = logger or logging.getLogger()
 
     # Open serial ports
-    ser1 = Serial(dev1_serial, 115200, timeout=2, inter_byte_timeout=0)
-    ser2 = Serial(dev2_serial, 115200, timeout=2, inter_byte_timeout=0)
     if platform == "linux":
-        subprocess.run(["stty", "-F", dev1_serial, "ignbrk", "echo"])
-        subprocess.run(["stty", "-F", dev2_serial, "ignbrk", "echo"])
+        subprocess.run(["stty", "-F", dev1_serial, "brkint", "noflsh"])
+        subprocess.run(["stty", "-F", dev2_serial, "brkint", "noflsh"])
+    ser1 = Serial(dev1_serial, 115200, timeout=2, inter_byte_timeout=0)
     ser1.reset_input_buffer()
+
+    ser2 = Serial(dev2_serial, 115200, timeout=2, inter_byte_timeout=0)
     ser2.reset_input_buffer()
 
     logger.info(f"Connected to bootloaders on {dev1_serial} and {dev2_serial}")
@@ -388,9 +389,9 @@ class Port:
         # If not connected, try to connect to serial device
         if not self.ser:
             try:
-                ser = Serial(self.device_serial, baudrate=self.baudrate, timeout=0.1, inter_byte_timeout=0)
                 if platform == "linux":
-                    subprocess.run(["stty", "-F", self.device_serial, "ignbrk", "echo"])
+                    subprocess.run(["stty", "-F", self.device_serial, "brkint", "noflsh"])
+                ser = Serial(self.device_serial, baudrate=self.baudrate, timeout=0.1, inter_byte_timeout=0)
                 ser.reset_input_buffer()
                 self.ser = ser
                 self.logger.info(f"Connection opened on {self.device_serial}")
